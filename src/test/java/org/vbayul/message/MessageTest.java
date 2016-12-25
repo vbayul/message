776084be676ce08e191fea9cb20438a6e6438ed5
Владимир <vbayul@gmail.com>
2016-12-25 17:58:44 +0200

    Log

--- src/test/java/org/vbayul/message/MessageTest.java
+++ src/test/java/org/vbayul/message/MessageTest.java
@@ -4,12 +4,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 import junit.framework.TestCase;
 
 public class MessageTest extends TestCase {
-
+	Logger log = Logger.getLogger(MessageTest.class.getName());
 	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 	Date testDate1 = new Date();
 	Date testDate2 = new Date();
@@ -19,6 +20,9 @@ public class MessageTest extends TestCase {
 	@Test
 	public void testResourceRU()
 	{
+		log.info("Begin test of resources of Russian languche");
+		log.info("Prepare resources");
+		
 		Resources res = new Resources();
 		ResourceBundle rb = res.getBundels("ru");
 		
@@ -27,31 +31,44 @@ public class MessageTest extends TestCase {
 		String result3 = rb.getString("part3");
 		String result4 = rb.getString("part4");
 		
+		log.info("Сompare results");
+		
 		assertEquals("Доброе утро, Мир!", result1);
 		assertEquals("Добрый день, Мир!", result2);
 		assertEquals("Добрый вечер, Мир!", result3);
 		assertEquals("Доброй ночи, Мир!", result4);
+		log.info("Test of resources of Russian languche successful");
 	}
 	
 	@Test
 	public void testResourceDefaultEN()
 	{
+		log.info("Begin test of resources of English/Default languche");	
+		
+		log.info("Prepare resources");
 		Resources res = new Resources();
 		ResourceBundle rb = res.getBundels("en");
+		
 		String result1 = rb.getString("part1");
 		String result2 = rb.getString("part2");
 		String result3 = rb.getString("part3");
 		String result4 = rb.getString("part4");
 		
+		log.info("Сompare results");
+		
 		assertEquals("Good morning, World!", result1);
 		assertEquals("Good day, World!", result2);
 		assertEquals("Good evening, World!", result3);
 		assertEquals("Good night, World!", result4);
+		log.info("Test of resources of English/Default languche successful");
 	}
 
 	@Test
 	public void testKeys()
 	{
+		log.info("Test of Keys method begin");
+		log.info("Prepare variables");
+		
 		try 
 		{
 			testDate1 = sdf.parse("07:00:00");
@@ -64,22 +81,30 @@ public class MessageTest extends TestCase {
 			e.printStackTrace();
 		}
 		
+		log.info("Prepare method keys");		
 		Keys keys = new Keys();
 		
 		String key1 = keys.getKey(testDate1);
 		String key2 = keys.getKey(testDate2);
 		String key3 = keys.getKey(testDate3);
 		String key4 = keys.getKey(testDate4);
+		
+		log.info("Сompare results");
 		
 		assertEquals("part1", key1);
 		assertEquals("part2", key2);
 		assertEquals("part3", key3);
 		assertEquals("part4", key4);
+		
+		log.info("Test of Keys method successful");
 	}
 	
 	@Test
 	public void testParts()
 	{
+		log.info("Test of  begin");
+		log.info("Prepare variables");
+		
 		try 
 		{
 			testDate1 = sdf.parse("07:00:00");
@@ -92,6 +117,7 @@ public class MessageTest extends TestCase {
 			e.printStackTrace();
 		}
 		
+		log.info("Prepare method keys");
 		Keys keys = new Keys();
 		
 		String key1 = keys.getKey(testDate1);
@@ -99,18 +125,22 @@ public class MessageTest extends TestCase {
 		String key3 = keys.getKey(testDate3);
 		String key4 = keys.getKey(testDate4);
 		
+		log.info("Prepare resource");
 		Resources rosources = new Resources();
 		ResourceBundle bundels = rosources.getBundels("en");
-		
+		log.info("Сompare results");
 		assertEquals("Good morning, World!", bundels.getString(key1));
 		assertEquals("Good day, World!", bundels.getString(key2));
 		assertEquals("Good evening, World!", bundels.getString(key3));
 		assertEquals("Good night, World!", bundels.getString(key4));
+		log.info("Test of random parts is successful");
 	}
 	
 	@Test
 	public void testPart1BorderSixToNineteen()
 	{
+		log.info("Test of border between  6-9 and 9-19 begin");
+		log.info("Prepare variables");
 		try 
 		{
 			testDate1 = sdf.parse("06:00:00");
@@ -122,7 +152,7 @@ public class MessageTest extends TestCase {
 		{
 			e.printStackTrace();
 		}
-		
+		log.info("Prepare method keys");
 		Keys keys = new Keys();
 		
 		String key1 = keys.getKey(testDate1);
@@ -130,18 +160,22 @@ public class MessageTest extends TestCase {
 		String key3 = keys.getKey(testDate3);
 		String key4 = keys.getKey(testDate4);
 		
+		log.info("Prepare resource");
 		Resources rosources = new Resources();
 		ResourceBundle bundels = rosources.getBundels("en");
 		
+		log.info("Сompare results");
 		assertEquals("Good morning, World!", bundels.getString(key1));
 		assertEquals("Good morning, World!", bundels.getString(key2));
 		assertEquals("Good day, World!", bundels.getString(key3));
 		assertEquals("Good day, World!", bundels.getString(key4));
+		log.info("Test of border between  6-9 and 9-19 successful");
 	}
 	
 	@Test
 	public void testPartBordersNineteenToSix()
 	{
+		log.info("Test of border between  19-23 and 23-6 begin");
 		try 
 		{
 			testDate1 = sdf.parse("19:00:00");
@@ -153,7 +187,7 @@ public class MessageTest extends TestCase {
 		{
 			e.printStackTrace();
 		}
-		
+		log.info("Prepare method keys");
 		Keys keys = new Keys();
 		
 		String key1 = keys.getKey(testDate1);
@@ -161,12 +195,15 @@ public class MessageTest extends TestCase {
 		String key3 = keys.getKey(testDate3);
 		String key4 = keys.getKey(testDate4);
 		
+		log.info("Prepare resource");
 		Resources rosources = new Resources();
 		ResourceBundle bundels = rosources.getBundels("en");
 		
+		log.info("Сompare results");		
 		assertEquals("Good evening, World!", bundels.getString(key1));
 		assertEquals("Good evening, World!", bundels.getString(key2));
 		assertEquals("Good night, World!", bundels.getString(key3));
 		assertEquals("Good night, World!", bundels.getString(key4));
+		log.info("Test of border between  19-23 and 23-6 is successful");
 	}
 }
